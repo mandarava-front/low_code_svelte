9f59b1a4476b880a3491909728422107c526100f
mandarava-front <1463354862@qq.com>
2022-02-15 18:00:56 +0800

    refline

--- src/utils/refline.js
+++ src/utils/refline.js
@@ -1,5 +1,7 @@
 // 初始化辅助线
 
+import { query_selector_all } from "svelte/internal"
+
 let lines = {
     xt : null,
     xc : null,
@@ -23,7 +25,7 @@ export function initRefLines () {
         node.show = function(){
            this.style.display = 'block'
         }
-        node.hidden = function(){
+        node.hide = function(){
             this.style.display = 'none'
         }
         document.body.appendChild(node)
@@ -39,6 +41,99 @@ export class RefLine {
         }
     }
     check(draging,othersClsStr,parent = document.body){
-        console.log('draging',draging)  
+        const allOtherClsNode = query_selector_all(othersClsStr)
+        const dragingRect = draging.getBoundingClientRect()
+        const parentRect = parent.getBoundingClientRect()
+//Array.from() 把为数组转换成数组
+        Array.from(allOtherClsNode).forEach( item => {
+            item.classList.remove('.ref-line-active')
+            if(item == draging) return;
+
+            const { top, right, bottom , left, width, height} = item.getBoundingClientRect()
+
+            let dragWidthHalf = dragingRect.width / 2
+            let dragingHeightHalf = dragingRect.height / 2
+            let itemWidthHalf = width / 2
+            let itemHeightHalf = height / 2
+            
+
+            let conditions = {
+                top: [
+                  // xt-top
+                  {
+                    isNearly: this.__isNearly(dragRect.top, top),
+                    lineNode: lines.xt,
+                    lineValue: top,
+                    dragValue: top
+                  },
+                  // xt-bottom
+                  {
+                    isNearly: this.__isNearly(dragRect.bottom, top),
+                    lineNode: lines.xt,
+                    lineValue: top,
+                    dragValue: top - dragRect.height
+                  },
+                  // cx
+                  {
+                    isNearly: this.__isNearly(dragRect.top + dragHeightHalf, top + itemHeightHalf),
+                    lineNode: lines.xc,
+                    lineValue: top + itemHeightHalf,
+                    dragValue: top + itemHeightHalf - dragHeightHalf
+                  },
+                  // xb-top
+                  {
+                    isNearly: this.__isNearly(dragRect.bottom, bottom),
+                    lineNode: lines.xb,
+                    lineValue: bottom,
+                    dragValue: bottom - dragRect.height
+                  },
+                  // xb-bottom
+                  {
+                    isNearly: this.__isNearly(dragRect.top, bottom),
+                    lineNode: lines.xb,
+                    lineValue: bottom,
+                    dragValue: bottom
+                  }
+                ],
+                left: [
+                  {
+                    isNearly: this.__isNearly(dragRect.left, left),
+                    lineNode: lines.yl,
+                    lineValue: left,
+                    dragValue: left
+                  },
+                  {
+                    isNearly: this.__isNearly(dragRect.right, left),
+                    lineNode: lines.yl,
+                    lineValue: left,
+                    dragValue: left - dragRect.width
+                  },
+                  {
+                    isNearly: this.__isNearly(dragRect.left + dragWidthHalf, left + itemWidthHalf),
+                    lineNode: lines.yc,
+                    lineValue: left + itemWidthHalf,
+                    dragValue: left + itemWidthHalf - dragWidthHalf
+                  },
+                  {
+                    isNearly: this.__isNearly(dragRect.right, right),
+                    lineNode: lines.yr,
+                    lineValue: right,
+                    dragValue: right - dragRect.width
+                  },
+                  {
+                    isNearly: this.__isNearly(dragRect.left, right),
+                    lineNode: lines.yr,
+                    lineValue: right,
+                    dragValue: right
+                  }
+                ]
+              }
+                
+        } )
+    }
+
+    uncheck(){
+        Object.values(lines).forEach( item => item.hide())
+
     }
 }
